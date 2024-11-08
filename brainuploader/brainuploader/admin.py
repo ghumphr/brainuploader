bc61ceafc4e4545eab1d60b388e2e4ba759d0ce1
Geoffrey Humphreys <ghumphreys@unomaha.edu>
2024-11-08 00:41:07 -0500

    adding flashcard organization api, moved static files folder

--- brainuploader/brainuploader/admin.py
+++ brainuploader/brainuploader/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Flashcard
 
 class FlashcardAdmin(admin.ModelAdmin):
-    list_display = ('user', 'last_right', 'times_right_in_a_row', 'front', 'back')
-    search_fields = ('user', 'last_right', 'times_right_in_a_row', 'front', 'back')
-    list_filter = ('user', 'last_right', 'times_right_in_a_row', 'front', 'back')
-    list_filter = ('user', 'last_right', 'times_right_in_a_row', 'front', 'back')
-    list_display_links = ('user', 'last_right', 'times_right_in_a_row', 'front', 'back')
-    fields = ('user', 'last_right', 'times_right_in_a_row', 'front', 'back')
+    list_display = ('deck', 'last_right', 'times_right_in_a_row', 'front', 'back')
+    search_fields = ('deck', 'last_right', 'times_right_in_a_row', 'front', 'back')
+    list_filter = ('deck', 'last_right', 'times_right_in_a_row', 'front', 'back')
+    list_filter = ('deck', 'last_right', 'times_right_in_a_row', 'front', 'back')
+    list_display_links = ('deck', 'last_right', 'times_right_in_a_row', 'front', 'back')
+    fields = ('deck', 'last_right', 'times_right_in_a_row', 'front', 'back')
     
 admin.site.register(Flashcard, FlashcardAdmin)
 
--- brainuploader/brainuploader/models.py
+++ brainuploader/brainuploader/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class Flashcard(models.Model):
+
+class Deck(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=127)
+    description = models.CharField(max_length=4094)
+
+    def __str__(self):
+        return self.name
+
+class Flashcard(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
     category = models.CharField(max_length=127)
     last_right = models.DateTimeField()
     times_right_in_a_row = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(30)])
