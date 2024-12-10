3b62b6ff25cd365a8a4d6893523265b223d2d7d8
Geoffrey Humphreys <ghumphreys@unomaha.edu>
2024-12-10 17:43:28 -0500

    made several fixes to CRUD api

--- api-server/app/brainuploader/serializers.py
+++ api-server/app/brainuploader/serializers.py
@@ -12,3 +12,49 @@ class DeckSerializer(serializers.ModelSerializer):
         model = Deck
         fields = ['id', 'user', 'name', 'description',]
 
+class OwnerFlashcardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flashcard
+        # Note: the deck field requires careful validation on modification
+        fields = ['next_review', 'times_right_in_a_row', 'front', 'back', 'deck']
+        read_only_fields = ['id',]
+
+class PublicFlashcardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flashcard
+        fields = []
+        read_only_fields = ['front', 'back', 'deck', 'id',]
+
+class StaffFlashcardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flashcard
+        fields = []
+        read_only_fields = ['front', 'back', 'deck', 'id',]
+
+class AdminFlashcardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flashcard
+        fields = '__all__'
+
+class OwnerDeckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deck
+        fields = ['user', 'name', 'description',]
+        read_only_fields = ['id',]
+
+class PublicDeckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deck
+        fields = []
+        read_only_fields = ['id', 'user', 'name', 'description',]
+
+class StaffDeckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deck
+        fields = []
+        read_only_fields = ['id', 'user', 'name', 'description',]
+
+class AdminDeckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deck
+        fields = '__all__'
--- api-server/app/brainuploader/views.py
+++ api-server/app/brainuploader/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from .permissions import IsOwner
-from .permissions import IsDeckOwner
+from brainuploader.permissions import IsOwner
+from brainuploader.permissions import IsDeckOwner
 from rest_framework import generics
-
-# Create your views here.
-
+from django.db.models import Q
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from brainuploader.models import Deck
@@ -18,21 +16,41 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+# Create your views here.
+
 # We plan to rely on the Django REST framework, which automatically escapes HTML
 # entities. This is much less tedious and error-prone than doing it ourselves.
 
 
-
+# This is used for the user signup
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
+
 # see for more details on generics: https://www.django-rest-framework.org/api-guide/generic-views/
 
 class FlashcardViewSet(viewsets.ModelViewSet):
-#    queryset = Flashcard.objects.all()
     serializer_class = FlashcardSerializer
+
+#    def get_queryset(self):
+#        """
+#        This view should return a list of all the flashcards in decks
+#        owned by the currently authenticated user or in public decks.
+#        """
+#        user = self.request.user
+#        user_viewset = Flashcard.objects.none()
+#        if(user is not None):
+#            if(user.is_staff or user.is_superuser):
+#                user_viewset = Flashcard.objects.all()
+#            else:
+#                user_viewset = Flashcard.objects.all().filter(deck__user=user)
+#        else:
+#            user_viewset = Flashcard.objects.none()
+#        public_viewset = Flashcard.objects.all().filter(deck__is_public=True)
+#        return public_viewset.union(user_viewset)
+
 
     def get_queryset(self):
         """
@@ -40,34 +58,28 @@ class FlashcardViewSet(viewsets.ModelViewSet):
         owned by the currently authenticated user or in public decks.
         """
         user = self.request.user
-        user_viewset = Flashcard.objects.none()
         if(user is not None):
-                user_viewset = Flashcard.objects.all().filter(deck__user=user)
+            if(user.is_staff or user.is_superuser):
+                return Flashcard.objects.all()
+            else:
+                return Flashcard.objects.all().filter(Q(deck__is_public=True) | Q(deck__user=user))
         else:
-                user_viewset = Flashcard.objects.none()
-        public_viewset = Flashcard.objects.all().filter(deck__is_public=True)
-        return public_viewset.union(user_viewset)
+                return Flashcard.objects.all().filter(deck__is_public=True)
 
 
-#class FlashcardQueryAPIView(generics.ListAPIView):
-#    queryset = Flashcard.objects.all()
-#    serializer_class = FlashcardSerializer
-#    permission_classes = [permissions.IsAuthenticated]
 
 class DeckViewSet(viewsets.ModelViewSet):
-#    queryset = Deck.objects.all()
     serializer_class = DeckSerializer
+
     def get_queryset(self):
         """
         This view should return a list of all the decks
         owned by the currently authenticated user or public.
-        FIXME: this doesn't address public decks yet
         """
         user = self.request.user
+        if(user is not None):
+            if(user.is_staff or user.is_superuser):
+                return Deck.objects.all()
+        # Return all public decks and all user decks (even if user is None)
         return Deck.objects.all().filter(user==user).union(Deck.objects.all().filter(is_public=True))
 
-
-#class DeckQueryAPIView(generics.ListAPIView):
-#    queryset = Deck.objects.all()
-#    serializer_class = DeckSerializer
-#    permission_classes = [permissions.IsAuthenticated]
