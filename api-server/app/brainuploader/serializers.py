4db27366de6456c0515058e1732894e362e16b62
Geoffrey Humphreys <ghumphreys@unomaha.edu>
2024-12-13 05:57:17 -0500

    added CSS protection

--- api-server/app/brainuploader/serializers.py
+++ api-server/app/brainuploader/serializers.py
@@ -2,21 +2,39 @@ from rest_framework import serializers
 from brainuploader.models import Flashcard
 from brainuploader.models import Deck
 from django.core.exceptions import PermissionDenied
+from django.utils.html import escape
 
+
+# Base class for all Flashcard serializers
 class FlashcardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Flashcard
-        fields = ['id', 'deck', 'next_review', 'times_right_in_a_row', 'front', 'back',]
 
+    # No HTML is allowed in the internal representation of any field
+    # While it would be more elegant to store the data as-is and remove html
+    # tags on display, that is also more dangerous.
+    #
+    def to_internal_value(self, data):
+        data = super(FlashcardSerializer, self).to_internal_value(data)
+        for k in data.keys():
+            data[k] = escape(data[k])
+        return(data)
+
+
+# Base class for all Deck serializers
 class DeckSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Deck
-        fields = ['id', 'user', 'name', 'description',]
 
-class UserFlashcardSerializer(serializers.ModelSerializer):
+    # No HTML is allowed in the internal representation of any field
+    # While it would be more elegant to store the data as-is and remove html
+    # tags on display, that is also more dangerous.
+    #
+    def to_internal_value(self, data):
+        data = super(DeckSerializer, self).to_internal_value(data)
+        for k in data.keys():
+            data[k] = escape(data[k])
+        return(data)
+
+class UserFlashcardSerializer(FlashcardSerializer):
     class Meta:
         model = Flashcard
-        # Note: the deck field requires careful validation on modification
         fields = ['next_review', 'times_right_in_a_row', 'front', 'back', 'deck']
         read_only_fields = ['id',]
 
@@ -29,42 +47,42 @@ class UserFlashcardSerializer(serializers.ModelSerializer):
                 raise PermissionDenied("Illegal deck parameter.")
         return super(UserFlashcardSerializer, self).to_internal_value(data)
 
-class PublicFlashcardSerializer(serializers.ModelSerializer):
+class PublicFlashcardSerializer(FlashcardSerializer):
     class Meta:
         model = Flashcard
         fields = []
         read_only_fields = ['front', 'back', 'deck', 'id',]
 
-class StaffFlashcardSerializer(serializers.ModelSerializer):
+class StaffFlashcardSerializer(FlashcardSerializer):
     class Meta:
         model = Flashcard
         fields = []
         read_only_fields = ['front', 'back', 'deck', 'id',]
 
-class AdminFlashcardSerializer(serializers.ModelSerializer):
+class AdminFlashcardSerializer(FlashcardSerializer):
     class Meta:
         model = Flashcard
         fields = '__all__'
 
-class UserDeckSerializer(serializers.ModelSerializer):
+class UserDeckSerializer(DeckSerializer):
     class Meta:
         model = Deck
         fields = ['user', 'name', 'description',]
         read_only_fields = ['id',]
 
-class PublicDeckSerializer(serializers.ModelSerializer):
+class PublicDeckSerializer(DeckSerializer):
     class Meta:
         model = Deck
         fields = []
         read_only_fields = ['id', 'user', 'name', 'description',]
 
-class StaffDeckSerializer(serializers.ModelSerializer):
+class StaffDeckSerializer(DeckSerializer):
     class Meta:
         model = Deck
         fields = []
         read_only_fields = ['id', 'user', 'name', 'description',]
 
-class AdminDeckSerializer(serializers.ModelSerializer):
+class AdminDeckSerializer(DeckSerializer):
     class Meta:
         model = Deck
         fields = '__all__'
