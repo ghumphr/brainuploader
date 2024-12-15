9e2ed44fdfa81016969bb0260099843081e8ca7e
Geoffrey Humphreys <geoffrey.g.humphreys@gmail.com>
2024-12-15 01:59:02 -0500

    deleted backup file

--- api-server/app/brainuploader/permissions.py
+++ api-server/app/brainuploader/permissions.py
@@ -1,37 +1,5 @@
 from rest_framework import permissions
 
-class IsOwner(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to view it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
-
-class IsReadable(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to view it unless it is public.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return obj.is_public or obj.user == request.user
-
-class IsDeckOwner(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to view it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return obj.deck.user == request.user
-
-class IsDeckReadable(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to view it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return obj.deck.is_public or obj.user == request.user
-
 class CanAccessDeck(permissions.BasePermission):
     """
     Custom permission to allow full access to a deck to owner and superuser, read-only access to
